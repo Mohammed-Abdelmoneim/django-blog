65179c9155d933d4df6e0ff1aa9cce7faa5f26fa
Mohammed Abdelmoneim <mohamedabdelmoneim95@gmail.com>
2025-06-21 02:25:29 +0300

    add: comment system

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Post, CommentForm
+from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
@@ -41,7 +41,12 @@ def post_detail(request, year, month, day, post): # post here means slug that's 
         publish__year=year,
         publish__month=month,
         publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -101,5 +106,5 @@ def post_comment(request, post_id):
         comment.post = post
         # Save the comment to the databasa
         comment.save()
-    return render('request', 'blog/post/comment,html', 
+    return render('request', 'blog/post/comment.html', 
                   {'post': post, 'comment': comment, 'form': form})
