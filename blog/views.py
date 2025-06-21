7fcbeeddf0ca41c22c5b84f0f222fd1cf6445c8a
Mohammed Abdelmoneim <mohamedabdelmoneim95@gmail.com>
2025-06-21 18:41:21 +0300

    add: implement retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,8 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
+
 
 # Create your views here.
 def post_list(request, tag_slug=None):
@@ -47,10 +49,29 @@ def post_detail(request, year, month, day, post): # post here means slug that's 
         publish__day=day)
     # List of active comments for this post
     comments = post.comments.filter(active=True)
-    # Form for users to comment
-    form = CommentForm()
+    # Form for users to comment 
+    form = CommentForm() 
     
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    # List of similar posts 
+    post_tags_ids = post.tags.values_list('id', flat=True) # retrieve a list if IDs 
+    similar_posts = Post.published.filter(
+        tags__in =post_tags_ids # get all posts contain any of these tags
+    ).exclude(id=post.id) # exclude current post id to not sugg the same post
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+    
+    
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'form': form,
+            'similar_posts': similar_posts
+        }
+    )
 
 
 def post_share(request, post_id):
